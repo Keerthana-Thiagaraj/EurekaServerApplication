1d212121806f86604532557ef7ee42a9924b6e6b
keerthanacse13@gmail.com <keerthanacse13@gmail.com>
2020-06-28 16:21:07 +0530

    SeRver configuration-discovery service

---
+++ src/test/java/com/application/eurekaserver/EurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
